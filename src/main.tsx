d2852ced5dec9b658dbab2408e584ec396a0db80
pavitra0 <143537026+pavitra0@users.noreply.github.com>
2025-06-07 18:49:46 +0530

    Fixed #4 main.tsx
    Fixed #4 changed hello world in korean

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("my-app")!).render(<h1>Hello, World</h1>);
+ReactDOM.createRoot(document.getElementById("my-app")!).render(<h1>annyeong segye</h1>);
